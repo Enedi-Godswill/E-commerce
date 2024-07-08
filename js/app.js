8eac585db90d06a1179938f8cf05b48224842b1b
Enedi Godswill <bobbyenedi@gmail.com>
2024-07-08 19:31:28 +0100

    third commit

---
+++ js/app.js
@@ -0,0 +1,11 @@
+
+const menuEl = document.querySelector("#menu");
+const menuBtn = document.querySelector("#menu-btn");
+
+const toggleBtn = () => {
+    menuEl.classList.toggle("active");
+    menuBtn.classList.toggle("active");
+}
+
+menuBtn.addEventListener("click", toggleBtn);
+
